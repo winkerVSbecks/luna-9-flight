d18aecd8bf515dded97c67488e70cba3542c3a20
Varun Vachhar <varunvachhar@gmail.com>
2017-01-29 14:18:14 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var sunrays = selectAll('.sunray');
+
+/**
+ * Animate sunrays
+ */
+var sunraysTimeline = anime.timeline({ loop: true });
+
+var sunrayAnimPart1 = anime({
+  targets: sunrays,
+  strokeDashoffset: [anime.setDashoffset, 0],
+  delay: function(_, idx) { return idx * 60; },
+  duration: 1800,
+  easing: 'easeOutExpo',
+  autoPlay: false,
+  complete: () => sunrayAnimPart2.play()
+});
+sunrayAnimPart1.pause();
+
+var sunrayAnimPart2 = anime({
+  targets: sunrays,
+  strokeDashoffset: [0, (el) => -1 * anime.setDashoffset(el)],
+  delay: function(_, idx) { return idx * 60; },
+  duration: 1800,
+  easing: 'easeInExpo',
+  complete: () => sunrayAnimPart1.play()
+});
+
+/**
+ * Utils
+ */
+function select(query) {
+  return document.querySelector(query);
+}
+
+function selectAll(query) {
+  return document.querySelectorAll(query);
+}
